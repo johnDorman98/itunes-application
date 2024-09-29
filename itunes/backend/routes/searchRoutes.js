0f5d82b301f5aa08a6f7062760171fbdb9cae9f8
John Dorman <98963869+johnDorman98@users.noreply.github.com>
2024-09-29 18:50:29 +0200

    Delete itunes directory

--- itunes/backend/routes/searchRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const searchController = require('../controllers/searchController');
-const authenticateJWT = require('../middleware/authenticateJWT');
-
-// Route to search for a user at api/search
-router.get('/', authenticateJWT, searchController);
-
-module.exports = router;
